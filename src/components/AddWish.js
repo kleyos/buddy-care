5335a80cd185016dd75cc2fafe32082c2aed5fb2
LITS <lits@MacBook-Pro-LITS.local>
2018-01-19 18:25:01 +0200

    Add wish screen

--- src/components/AddWish.js
+++ src/components/AddWish.js
@@ -1,17 +1,10 @@
 import React, { Component } from 'react';
-import {
-	AppRegistry,
-	Text,
-	View, Image, TextInput, StyleSheet
-} from 'react-native';
-import { Button } from 'react-native-elements';
+import { Text, View, Image, TextInput, StyleSheet, Button } from 'react-native';
 
 import { StackNavigator, NavigationActions } from 'react-navigation';
 
 export default class AddWish extends Component {
-	static navigationOptions = {
-		title: null,
-	};
+
 	constructor(props) {
 		super(props);
 		this.state = {};
@@ -25,17 +18,24 @@ export default class AddWish extends Component {
 			.catch(error =>console.log(error) )
 	}
 
-	submit = () => {
+	submit = (dispatch) => {
 		console.log('submit!!!')
+		dispatch(NavigationActions.navigate({ routeName: 'Main', params: {} }))
 	}
 
 	render() {
 		const { navigate, dispatch } = this.props.navigation;
 		return (
 			<View style={styles.container}>
-				<View style={styles.row}>
-					<Text style={styles.label}>Well done! One more step</Text>
-					<Image />
+				<View style={{flex: 0.5}}>
+					<Text style={styles.welcome}>Well done! One more step.</Text>
+				</View>
+				<View style={{flex: 1, justifyContent: 'center', alignItems: 'center',  marginBottom: 10 }}>
+					<Image style={{width: 200, height: 200}}
+						source={require('../images/smile.jpg')}
+						resizeMode='contain'/>
+				</View>
+				<View style={{flex: 1,}}>
 					<Text style={styles.label}>Add your wish:</Text>
 					<TextInput
 						style={styles.input}
@@ -47,54 +47,49 @@ export default class AddWish extends Component {
 						style={styles.input}
 					 	onChangeText={(offer) => this.setState({...this.state, offer})}
 					 	value={this.state.offer} />
-			</View>
-			<View style={styles.btns}>
-				{/* <Button
-					onPress={this.submit}
-					title="SUBMIT"
-					color="#a52a2a" /> */}
-				<Button raised
-					onPress={this.submit}
-					icon={{name: 'home', size: 14}}
-					buttonStyle={styles.submitBtn}
-					textStyle={{textAlign: 'center'}}
-					title={`SUBMIT`}
-				/>
-				<Button
-					onPress={() =>
-						dispatch(NavigationActions.navigate({ routeName: 'Main', params: {} }))}
-					title="skip for now >>"
-					color="#008b8b" />
-			</View>
+				</View>
+				<View style={{flex: 1}}>
+					<Button
+						onPress={() => this.submit(dispatch)}
+						title="SUBMIT"
+						color="#a52a2a" />
+
+					<Button
+						onPress={() =>
+							dispatch(NavigationActions.navigate({ routeName: 'Main', params: {} }))}
+						title="skip for now >>"
+						color="#008b8b" />
+				</View>
 		</View>)
 	}
 }
+
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		justifyContent: 'center',
-		backgroundColor: '#F5FCFF',
+		backgroundColor: '#FFF',
 		padding:10,
 	},
-	submitBtn: {
-		backgroundColor: "#a52a2a",
-		borderRadius: 10,
-		width: 200,
+	welcome: {
+		fontSize: 25,
+		textAlign: 'center',
+		margin: 10,
+		fontFamily: 'Cochin',
 	},
 	label: {
-		fontSize: 18,
+		fontSize: 12,
 		margin: 5,
+		fontSize: 20,
+		textAlign: 'center',
 	},
 	input:{
 		height: 40,
 		width: 'auto',
 		textAlign:'left',
 		padding: 5,
-		borderWidth:2,
+		borderWidth:1,
 		borderColor:'#A4DFF9',
 		backgroundColor: 'transparent',
 	},
-	btns: {
-		marginTop: 15,
-	}
 });
--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -56,7 +56,7 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: 'center',
 		alignItems: 'center',
-		backgroundColor: '#F5FCFF',
+		backgroundColor: '#FFF',
 	},
 	welcome: {
 		fontSize: 40,
