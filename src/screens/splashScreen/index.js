5e4ae800c2ef0fa60657820eb2cde43e126afaae
kleyos <olga.erushenko@gmail.com>
2018-08-19 15:39:00 +0300

    Fix style of splash screen

--- src/screens/splashScreen/index.js
+++ src/screens/splashScreen/index.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 
 import { Image } from 'react-native';
-import styles from '../../components/headerProfile/styles';
+import styles from './styles';
 
 export default class SplashScreen extends Component {
   render() {
     return (<Image
       style={styles.splash}
       source={require('../../assets/splashScreen.png')}
+      resizeMode="stretch"
     />);
   }
 }
--- src/screens/splashScreen/styles.js
+++ src/screens/splashScreen/styles.js
@@ -1,11 +1,10 @@
 import { StyleSheet, Dimensions } from 'react-native';
 
-const { width, height } = Dimensions;
+const { width, height } = Dimensions.get('screen');
 
 export default StyleSheet.create({
   splash: {
     width,
     height
   }
-})
-;
+});
