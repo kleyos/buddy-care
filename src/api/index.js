7cd4a7fefcd43b8103a327e74ab114b4aae16707
kleyos <olga.erushenko@gmail.com>
2018-08-24 14:43:07 +0300

    Add api functions

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,64 @@
-const rootUrl = 'https://buddy-care.herokuapp.com/api/v1';
+const API_URL = 'https://buddy-care.herokuapp.com/api/v1';
+
+const get = url => fetch(url).then(res => res.json());
+
+const post = (url, data) =>
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: data
+  }).then(res => res.json());
+
+const put = (url, data) =>
+  fetch(url, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: data
+  }).then(res => res.json());
+
+const del = url =>
+  fetch(url, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  }).then(res => res.json());
 
 export const getUsers = () => {
-  return fetch(`${rootUrl}/cards`).then(res => res.json());
+  const url = `${API_URL}/cards`;
+  return get(url);
 };
 
 export const getUserProfile = userId => {
-  return fetch(`${rootUrl}/users/${userId}`).then(res => res.json());
+  const url = `${API_URL}/users/${userId}`;
+  return get(url);
 };
 
+export const createUser = token => {
+  const url = `${API_URL}/sessions`;
+  return post(url, { access_token: token });
+};
+
+export const create = (text, type) => {
+  const url = `${API_URL}/${type}`;
+  return post(url, { text });
+};
+
+export const edit = (text, type, id) => {
+  const url = `${API_URL}/${type}/:${id}`;
+  return put(url, { text });
+};
+
+export const apply = (id, type) => {
+  const url = `${API_URL}/${type}/:${id}/apply`;
+  return post(url);
+};
+
+export const remove = (id, type) => {
+  const url = `${API_URL}/${type}/:${id}`;
+  return del(url);
+};
