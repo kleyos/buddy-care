6243f2b8d17bca1494e159b3a9331a38fbf4871f
kleyos <olga.erushenko@gmail.com>
2018-08-24 14:42:05 +0300

    Splash screen improvements

--- src/screens/splashScreen/index.js
+++ src/screens/splashScreen/index.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 
-import { Image } from 'react-native';
+import { Image, ImageBackground } from 'react-native';
 import styles from './styles';
 
 export default class SplashScreen extends Component {
   render() {
-    return (<Image
-      style={styles.splash}
-      source={require('../../assets/splashScreen.png')}
-      resizeMode="stretch"
-    />);
+    return (
+      <ImageBackground
+        style={styles.container}
+        source={require('../../assets/loginBg.png')}>
+        <Image
+          style={styles.splash}
+          source={require('../../assets/logoWite.png')}
+          resizeMode="contain"
+        />
+      </ImageBackground>);
   }
 }
-
 SplashScreen.navigationOptions = {
   header: null
 };
--- src/screens/splashScreen/styles.js
+++ src/screens/splashScreen/styles.js
@@ -3,8 +3,15 @@ import { StyleSheet, Dimensions } from 'react-native';
 const { width, height } = Dimensions.get('screen');
 
 export default StyleSheet.create({
-  splash: {
+  container: {
+    flex: 1,
     width,
-    height
+    height,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  splash: {
+    height: 250,
+    width: 300
   }
 });
